a9576c658c81a036a9958af571585871d1a1a4a3
Vishakha-Arya <158750325+Vishakha-Arya@users.noreply.github.com>
2024-02-08 11:23:56 +0000

    conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,38 @@
-// node 01_basics/03_conversationOperations.js
-let score =33
+// node 01_basics/03_conversionOperations.js
+// let score ="vishu"
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+/*
+NAN(not a number)-type:number; value: NaN ;
+Null- type:object; value: 0;
+undefined- type:undefined; value: NaN;
+true/false - type:boolean; value: 1,0
+"33"=> 33
+"33abc"=>NaN
+*/
+
+// let isLoggedIn = 5
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+/*
+1=> true 
+0=> false
+"vishakha"=>true
+"" => false
+any number=> true
+*/
+
+let someNumber= 33
+
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
