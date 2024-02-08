1e0af3f0ca3d599d4ce30fcfb74608cfd095ae23
Vishakha-Arya <158750325+Vishakha-Arya@users.noreply.github.com>
2024-02-08 12:53:02 +0000

    operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 // node 01_basics/03_conversionOperations.js
-// let score ="vishu"
+let score ="vishu"
 
 // console.log(typeof score);
 // console.log(typeof (score));
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
@@ -18,9 +18,9 @@ true/false - type:boolean; value: 1,0
 "33abc"=>NaN
 */
 
-// let isLoggedIn = 5
+let isLoggedIn = 5
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
 
 /*
@@ -34,5 +34,49 @@ any number=> true
 let someNumber= 33
 
 let stringNumber= String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ************operations***************//
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2^3)
+// console.log(2**3)
+// console.log(2/2)
+
+/* ^ is not used for power. ** is used for power*/
+
+let str1 = "hello"
+let str2 =" vishakha"
+let str3 =str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2 +2)
+// console.log(1+ 2 +2)
+// console.log(1+2 +"2")
+// console.log((3+4)*5%3)
+
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2, num3
+
+num1= num2= num3= 2 +2
+
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter)
+
+// link to study:
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
