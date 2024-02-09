baebda357c4f060d204e225e12fe979b17faee18
Vishakha-Arya <158750325+Vishakha-Arya@users.noreply.github.com>
2024-02-09 11:20:25 +0000

    abc

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,16 @@
+// node 01_basics/04_comparisons.js
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+ console.log("2">1);
+ console.log("02">1);
+
+ console.log();
+ 
+ console.log();
+ console.log('');
+ 
